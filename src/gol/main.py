165e886e6a1fc8798022ed323e2adf30c968570e
Egal432 <stephan.kreidl@outlook.de>
2026-01-10 16:14:51 +0100

    added blinker to main

--- src/gol/main.py
+++ src/gol/main.py
@@ -1,6 +1,16 @@
+from gol.engine import GameOfLife
+
+
 def main():
-    print("Game of Life starting")
+    game = GameOfLife(5, 5)
+    game.toggle(1, 2)
+    game.toggle(2, 2)
+    game.toggle(3, 2)
+
+    print(game.grid)
+    game.step()
+    print(game.grid)
+
 
 if __name__ == "__main__":
     main()
-
